19ffbe0815f973f140eb455d2298e23a1be4ab14
Antonio Olivares <antonio.olivares1995@gmail.com>
2020-05-03 20:26:44 -0400

    got quill editor working with angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+
+//entry point into application
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-root', //html tag
+  templateUrl: './app.component.html', //html source
+  styleUrls: ['./app.component.css'], //app css source
 })
-export class AppComponent {
-  title = 'textbookPlatform';
+export class AppComponent implements OnInit {
+  editorForm: FormGroup;
+  output: string;
+
+  onSubmit() {
+    this.output = this.editorForm.get('editor').value;
+  }
+
+  ngOnInit() {
+    this.editorForm = new FormGroup({
+      editor: new FormControl(null),
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuillModule } from 'ngx-quill';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent], //declare angular components
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    QuillModule.forRoot(),
+    ReactiveFormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent], //declare app entry point
 })
-export class AppModule { }
+export class AppModule {}
